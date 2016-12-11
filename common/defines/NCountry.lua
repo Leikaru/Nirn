1239cebe9e17c24794961f5d95cc4f458ec9ec4b
Leikaru <dinohaydn@gmail.com>
2016-12-11 12:07:16 +0000

    Initial Commit
    All inital files added.

---
+++ common/defines/NCountry.lua
@@ -0,0 +1,15 @@
+NDefines.NCountry.MONARCH_DEATH = 0 -- Deaths are handled through events. Can still die in combat. Immortal trait is just symbolic now.
+NDefines.NCountry.HEIR_DEATH = 0 -- Same as MONARCH_DEATH
+
+NDefines.NCountry.PRESTIGE_GAIN_FOR_GOV_RANK_2 = 0			-- Prestige gained on upgrading to government rank 2
+NDefines.NCountry.PRESTIGE_GAIN_FOR_GOV_RANK_3 = 0			-- Prestige gained on upgrading to government rank 3
+NDefines.NCountry.MIN_PRESTIGE_FOR_GOV_RANK_2 = 0			-- Required prestige to to upgrade to government rank 2
+NDefines.NCountry.MIN_PRESTIGE_FOR_GOV_RANK_3 = 0	-- Required prestige to to upgrade to government rank 3
+NDefines.NCountry.MIN_DEVELOPMENT_FOR_GOV_RANK_2 = 99999		-- Need at least this much development to upgrade to government rank 2
+NDefines.NCountry.MIN_DEVELOPMENT_FOR_GOV_RANK_3 = 99999	-- Need at least this much development to upgrade to government rank 3
+
+NDefines.NCountry.PRESTIGE_GAIN_FOR_GOV_RANK_X = 0			-- Prestige gained on upgrading to government rank above 3
+NDefines.NCountry.MIN_PRESTIGE_FOR_GOV_RANK_X = 0			-- Required prestige to to upgrade to government above 3
+NDefines.NCountry.ADDITIONAL_MIN_DEVELOPMENT_FOR_GOV_RANK_X = 0	-- ADDITIONAL development needed to upgrade to government rank above 3
+NDefines.NCountry.MAX_GOV_RANK = 7						-- Max possible is 10
+NDefines.NCountry.CULTURAL_UNION_MIN_RANK = 6			-- Must be an Empire in order to be a cultural union
